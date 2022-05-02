b1940f9de034bb2d1295f39e286db8de3bc8907e
Dmitry Korzun <korzundm7@gmail.com>
2022-05-02 16:42:55 +0300

    feat: add remove-from-list

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -1,6 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
-
-// const { ListNode } = require('../extensions/list-node.js');
+const { ListNode } = require('../extensions/list-node.js');
 
 /**
  * Given a singly linked list of integers l and an integer k,
@@ -22,10 +20,6 @@ const { NotImplementedError } = require('../extensions/index.js');
  *   }
  * }
  */
-function ListNode(x) {
-  this.value = x;
-  this.next = null;
-}
 
 function removeKFromList(l, k) {
   let total = null;
